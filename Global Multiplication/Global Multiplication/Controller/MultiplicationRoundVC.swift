a9e2eabcaa5602d877a9f40a7a41e365737e2b38
Jason <jason@sphere.ms>
2017-12-21 18:33:35 -0700

    Timer start/finished

--- Global Multiplication/Global Multiplication/Controller/MultiplicationRoundVC.swift
+++ Global Multiplication/Global Multiplication/Controller/MultiplicationRoundVC.swift
@@ -11,32 +11,50 @@ import UIKit
 class MultiplicationRoundVC: UIViewController {
     
     @IBOutlet var timerLabel: UILabel!
-    @IBOutlet var startStopSegment: UISegmentedControl!
+    @IBOutlet var correctScore: UILabel!
+    @IBOutlet var wrongScore: UILabel!
+    @IBOutlet var startPressed: UIButton!
+    @IBOutlet var finishedPressed: UIButton!
     
-    var timer: Timer?
-    var roundTimer: Double = 0.0
+    var counter = 0.00
+    var timer = Timer()
+    var isPlaying = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Round"
         
-        self.timerLabel.text = "20:00"
-
+        timerLabel.text = String(counter)
+        finishedPressed.isEnabled = false
+        
     }
     
-    func updateTimer() {
-        roundTimer = roundTimer + 1
-        if roundTimer == 20.00 {
-            timer?.invalidate()
-            
-            self.performSegue(withIdentifier: "SummarySegue", sender: self)
+    @IBAction func startTimer(_ sender: UIButton) {
+        if(isPlaying) {
+            return
         }
         
-        self.timerLabel.text = "\(timer)"
+        startPressed.isEnabled = false
+        finishedPressed.isEnabled = true
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(SaveTimer), userInfo: nil, repeats: true)
+        isPlaying = true
     }
     
-
+    @objc func SaveTimer() {
+        
+        counter = counter + 0.1
+        timerLabel.text = String(format: "%.1f", counter)
+        
+    }
     
-
+    @IBAction func finishedTimer(_ sender: UIButton) {
+        
+        startPressed.isEnabled = true
+        finishedPressed.isEnabled = false
+        
+        timer.invalidate()
+        isPlaying = false
+    }
 }
